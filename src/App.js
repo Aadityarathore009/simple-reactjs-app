03fdd3f1bf6765aaec4ace90da6494ea1bf0e836
Aadityarathore009 <rathoreaaditya810@gmail.com>
2025-03-16 14:57:36 +0530

    Adding power bi dashboard

--- src/App.js
+++ src/App.js
@@ -16,6 +16,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Simple React App</h1>
         </header>
+        <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h1>My Power BI Dashboard</h1>
+      <iframe title="scms dashboard" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=cb39a852-7fa2-403f-9482-5778b9649763&autoAuth=true&ctid=d988180a-487c-497e-b3d8-ee0146495fcb" frameborder="0" allowFullScreen="true"></iframe>
+    </div>
           <Switch>
                 <Route exact path= "/" render={() => (
                   <Redirect to="/customerlist"/>
